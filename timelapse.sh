f76f271643a1e9ad9af15c86ad14e969193f035a
EvanG <evan_g@orange.fr>
2022-09-14 22:05:02 +0200

    Time-lapse archive + 10 FPS video

--- timelapse.sh
+++ timelapse.sh
@@ -1,2 +1,3 @@
-ffmpeg -y -framerate 5 -pattern_type glob -i 'bak/*.png' -c:v libx264 -pix_fmt yuv420p -vf "pad=width=512:height=256:color=black" web/root/timelapse.mp4
+zip web/root/archive.zip bak/*.png
+ffmpeg -y -framerate 10 -pattern_type glob -i 'bak/*.png' -c:v libx264 -pix_fmt yuv420p -vf "pad=width=512:height=512:color=black" web/root/timelapse.mp4
 #ffmpeg -y -i web/root/timelapse.mp4 web/root/timelapse.webm
