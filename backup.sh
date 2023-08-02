380727ec73cc3951b8ae89033b94ae6c7e511e85
EvanG <evan_g@orange.fr>
2023-08-02 14:48:30 +0200

    feat: Intelligent backup system Only backup if the canvas has changed

--- backup.sh
+++ backup.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 echo "================ Backup ==============="
-cd /portainer/Files/AppData/Config/rPlace/
-cp place.png bak/$(date '+%Y-%m-%d_%H-%M-%S').png
-echo "Backup created"
+mkdir -p "bak/$(date '+%Y-%m')"
+rm bak/temp.png
+cp place.png bak/temp.png
+lastMd=$(md5sum "$(ls -t bak/*/*.png | head -n1)" | cut -d " " -f 1)
+if [[ $(md5sum bak/temp.png | cut -d " " -f 1) != "$lastMd" ]]; then
+	mv bak/temp.png "bak/$(date '+%Y-%m')/$(date '+%Y-%m-%d_%H-%M-%S').png"
+	echo "Backup created"
+fi
 
 echo "============== Timelapse =============="
 ./timelapse.sh
