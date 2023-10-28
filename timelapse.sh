0fb16e2cac423a0caba3d564aba8ed673d446bf3
EvanG <evan_g@orange.fr>
2023-10-29 00:17:22 +0200

    feat: Timelapse

--- timelapse.sh
+++ timelapse.sh
@@ -1,3 +1,6 @@
-zip web/root/archive.zip bak/*.png
-ffmpeg -y -framerate 10 -pattern_type glob -i 'bak/*.png' -c:v libx264 -pix_fmt yuv420p -vf "pad=width=512:height=512:color=black" web/root/timelapse.mp4
-#ffmpeg -y -i web/root/timelapse.mp4 web/root/timelapse.webm
+#!/bin/bash
+for month in bak/*/; do
+  zip "web/root/archives/archive-$(basename "$month").zip" "$month/*.png"
+done
+ffmpeg -framerate 30 -f image2 -export_path_metadata 1 -pattern_type glob -i 'bak/*/*.png' -vf "pad=width=1024:height=768:color=black" -y bak/timelapse.mkv
+ffmpeg -i bak/timelapse.mkv -y web/root/timelapse.mp4
