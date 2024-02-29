726bfd457cc4431f4be4b74ffc743b26e8d970b8
Evan Galli <evan_g@orange.fr>
2024-02-29 21:48:15 +0100

    fix: Change timelapse subsampling to 4:2:0 so it's supported by Firefox

--- timelapse.sh
+++ timelapse.sh
@@ -7,4 +7,4 @@ for month in bak/*/; do
 done
 echo "========== Video =========="
 ffmpeg -framerate 30 -f image2 -export_path_metadata 1 -pattern_type glob -i 'bak/*/*.png' -vf "pad=width=1024:height=768:color=black" -y bak/timelapse.mkv
-ffmpeg -i bak/timelapse.mkv -y web/root/timelapse.mp4
+ffmpeg -i bak/timelapse.mkv -pix_fmt yuv420p -y web/root/timelapse.mp4
