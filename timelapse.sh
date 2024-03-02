5f518496c81b4ee5e689339a69e042bfd5319936
Evan Galli <evan_g@orange.fr>
2024-03-02 16:23:45 +0100

    fix: ffmpeg was stopping globbing for no reason at the 10248th frame

--- timelapse.sh
+++ timelapse.sh
@@ -6,5 +6,7 @@ for month in bak/*/; do
   zip -jr "web/root/archives/archive-$(basename "$month").zip" "$month" -i "*.png"
 done
 echo "========== Video =========="
-ffmpeg -framerate 30 -f image2 -export_path_metadata 1 -pattern_type glob -i 'bak/*/*.png' -vf "pad=width=1024:height=768:color=black" -y bak/timelapse.mkv
+echo "====== 1st pass ======"
+cat bak/*/*.png | ffmpeg -framerate 30 -f image2pipe -i - -vf "pad=width=1024:height=768:color=black,format=yuv420p" -y bak/timelapse.mkv
+echo "====== 2nd pass ======"
 ffmpeg -i bak/timelapse.mkv -pix_fmt yuv420p -y web/root/timelapse.mp4
